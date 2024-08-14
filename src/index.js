cd8b9440534289febad97973668f667a249125c5
Kaleb <Kalebalebachew4@gmail.com>
2024-08-14 09:50:09 +0300

    basic usage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import HttpEase from "./http-ease.js";
 
+const ease = new HttpEase(); 
+
+ease.get("https://jsonplaceholder.typicode.com/posts")
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
+
 export default HttpEase;
